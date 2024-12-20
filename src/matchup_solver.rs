cd4291e3f117e746dc38a7e2eb6f4c4052ab80ad
Jan Luijk <luijk.jan@gmail.com>
2024-12-20 20:30:48 +0100

    code refactoring
    move methods and use Result instead of i32

--- src/matchup_solver.rs
+++ src/matchup_solver.rs
@@ -24,7 +24,7 @@ impl<'a> MatchupSolver<'a> {
         }
     }
 
-    pub fn use_pair_method(&mut self) {
+    pub fn _use_pair_method(&mut self) {
         self.method = Method::PerPair;
     }
 
@@ -35,32 +35,78 @@ impl<'a> MatchupSolver<'a> {
     fn solve_cells(&mut self) -> bool {
         let rows = self.matchup.rows;
         let cols = self.matchup.cols;
+        let max_cell_nr = rows * cols;
 
+        let mut matches = vec![true; max_cell_nr];
         let mut cellnr = 0;
         let mut start_from_match = 0;
-        let max_cell_nr = rows * cols;
-        let max_matches = rows * cols;
-        let mut matches = vec![true; max_matches];
 
         while cellnr < max_cell_nr {
-            let match_found = self.solve_cell(&mut matches, cellnr, start_from_match);
-            let no_match_found = match_found == -1;
-
-            if no_match_found {
+            if self.solve_cell(&mut matches, cellnr, start_from_match) {
+                cellnr += 1;
+                start_from_match = 0;
+            } else {
                 cellnr -= 1;
                 let previous_match = self.prev_cell_to_match(&mut matches, cellnr);
                 start_from_match = previous_match;
-            } else {
-                cellnr += 1;
-                start_from_match = 0;
-            }
-
-            if cellnr > cols * (rows - 1) {
-                self.matchup.print();
             }
         }
-
         true
+    }
+
+    fn solve_cell(&mut self, matches: &mut [bool], cellnr: usize, start_match: usize) -> bool {
+        let row = cellnr / self.matchup.cols;
+        let col = cellnr % self.matchup.cols; // probably optimised by compiler
+
+        let available = self.get_available(row, col);
+        if let Some(new_match) = self.get_first_match(matches, available, start_match) {
+            let (left, right) = self.match_to_data(new_match);
+            let cell = &mut self.matchup.data[row][col];
+            cell.left = left;
+            cell.right = right;
+            true
+        } else {
+            false
+        }
+    }
+
+    fn get_available(&self, curr_row: usize, curr_col: usize) -> u32 {
+        let mut available = self.get_available_bitmask();
+
+        for c in 0..curr_col {
+            let cell = self.matchup.data[curr_row][c];
+
+            self.set_value(cell.left, &mut available);
+            self.set_value(cell.right, &mut available);
+        }
+
+        let mut range = 0..curr_row;
+        let second_half = curr_row >= self.matchup.cols;
+
+        if second_half {
+            range = self.matchup.cols..curr_row;
+        }
+
+        for r in range {
+            let cell = self.matchup.data[r][curr_col];
+
+            self.set_value(cell.left, &mut available);
+            self.set_value(cell.right, &mut available);
+        }
+
+        available
+    }
+
+    #[inline]
+    fn get_available_bitmask(&self) -> u32 {
+        ((1u32 << (self.matchup.rows + 2)) - 1) & !1
+    }
+
+    #[inline]
+    fn set_value(&self, value: usize, available: &mut u32) {
+        if value != 0 {
+            *available &= !(1 << value);
+        }
     }
 
     fn prev_cell_to_match(&self, matches: &mut [bool], cellnr: usize) -> usize {
@@ -110,89 +156,29 @@ impl<'a> MatchupSolver<'a> {
         (left, right)
     }
 
-    fn solve_cell(&mut self, matches: &mut [bool], cellnr: usize, start_match: usize) -> i32 {
-        let curr_row = cellnr / self.matchup.cols;
-        let curr_col = cellnr % self.matchup.cols; // probably optimised by compiler
+    fn get_first_match(
+        &self,
+        matches: &mut [bool],
+        available: u32,
+        start_match: usize,
+    ) -> Option<usize> {
+        (start_match..self.matchup.rows * self.matchup.cols).find(|&matchnr| {
+            if matches[matchnr] {
+                let (left, right) = self.match_to_data(matchnr);
 
-        let available = self.get_available(curr_row, curr_col);
-        let new_match = self.get_first_match(matches, available, start_match);
+                let left_available = (available & (1 << left)) != 0;
+                let right_available = (available & (1 << right)) != 0;
 
-        if new_match >= 0 {
-            let data = self.match_to_data(new_match as usize);
-            self.matchup.data[curr_row][curr_col].left = data.0;
-            self.matchup.data[curr_row][curr_col].right = data.1;
-        }
-
-        new_match
-    }
-
-    fn get_first_match(&self, matches: &mut [bool], available: u32, start_match: usize) -> i32 {
-        let max_matches = self.matchup.rows * self.matchup.cols;
-
-        let mut matchnr = start_match;
-
-        while matchnr < max_matches {
-            if !matches[matchnr] {
-                matchnr += 1;
-                continue;
+                if left_available && right_available {
+                    matches[matchnr] = false;
+                    return true;
+                }
             }
-
-            let pair = self.match_to_data(matchnr);
-            let left = pair.0;
-            let right = pair.1;
-
-            let right_available = (available & (1 << right)) != 0;
-            let left_available = (available & (1 << left)) != 0;
-
-            if left_available && right_available {
-                matches[matchnr] = false;
-
-                return matchnr as i32;
-            }
-
-            matchnr += 1;
-        }
-        -1
-    }
-
-    fn get_available(&self, curr_row: usize, curr_col: usize) -> u32 {
-        let mut available = self.get_available_bitmask();
-
-        for c in 0..curr_col {
-            let entry = self.matchup.data[curr_row][c];
-
-            self.set_value(entry.left, &mut available);
-            self.set_value(entry.right, &mut available);
-        }
-
-        let mut range = 0..curr_row;
-        let second_half = curr_row >= self.matchup.cols;
-
-        if second_half {
-            range = self.matchup.cols..curr_row;
-        }
-
-        for r in range {
-            let entry = self.matchup.data[r][curr_col];
-
-            self.set_value(entry.left, &mut available);
-            self.set_value(entry.right, &mut available);
-        }
-
-        available
+            false
+        })
     }
 
     fn solve_pairs(&mut self) -> bool {
         true
-    }
-
-    fn set_value(&self, value: usize, available: &mut u32) {
-        if value != 0 {
-            *available &= !(1 << value);
-        }
-    }
-
-    fn get_available_bitmask(&self) -> u32 {
-        ((1u32 << (self.matchup.rows + 2)) - 1) & !1
     }
 }
